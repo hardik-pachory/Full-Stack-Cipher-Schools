5c673352a53c45c96dc69ea5e06537f6fe7cd2d6
Hardik Pachory <84628216+hardik-pachory@users.noreply.github.com>
2021-06-09 22:30:36 +0530

    Add files via upload

---
+++ Day 14/index.js
@@ -0,0 +1,58 @@
+//const { default: axios } = require('axios');
+const http = require('http');
+const axios = require('axios')
+const { url } = require('inspector');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+var urli = 'https://pokeapi.co/api/v2/pokemon/'
+
+async function makeGetRequest(ids) {
+
+    let res = await axios.get('https://pokeapi.co/api/v2/pokemon/' + ids);
+
+    let data = res.data;
+    console.log(data);
+}
+for (let i = 1; i <= 10; i++) {
+    console.log('running loop ' + i);
+    makeGetRequest(i);
+}
+// async function getPokemon() {
+//     let url = 'https://pokeapi.co/api/v2/pokemon/1'
+//     try {
+//         const response = await axios.get(url);
+//         return response;
+//     } catch (error) {
+//         console.error(error)
+//     }
+    
+// }
+
+
+
+
+// function getPokemon(id) {
+//     let url_id = urli + id;
+//     console.log(url_id)
+//     axios.get(url_id).then((response) => {
+//         return response;
+//     })
+//         .catch((error) => {
+//             console.error(error);
+//     })
+// }
+
+
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(pokemon);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
